4a0bab6307da456f87d0f6f073dad6d1aa70099f
fjzzq2002 <fjzzq2002@163.com>
2016-07-21 11:46:06 +0800

    changed name

--- gen/216.cpp
+++ gen/216.cpp
@@ -27,13 +27,19 @@ ll rp(ll x)
 	if(t>1000000000000000000LL||t<1) return rndll();
 	return t;
 }
+ll fib[2333];
 int main(){
+	fib[0]=fib[1]=1;
+	for(int i=2;i<=233;i++) fib[i]=fib[i-1]+fib[i-2];
 	int s;
-  freopen("s", "r", stdin); scanf("%d", &s); srand(s);
-  //ll A=rndll(),B=rndll()£»
-  ll A=864691128455135230,B=3;
-  ll g=gcd(A,B);
-  std::cout<<A<<" "<<B<<" "<<rp(g)<<"\n"; 
-  freopen("s", "w", stdout); printf("%d", rand());
-  return 0;
+	freopen("s", "r", stdin); scanf("%d", &s); srand(s);
+	//ll A=rndll(),B=rndll();
+	//ll A=864691128455135230,B=3;
+	//ll A=rndll(),B=rand()%2+2;
+	ll A=fib[rand()%4+83]+rand(),B=rand()%2+2;
+	if(rand()%4==0) B=1;
+	ll g=gcd(A,B);
+	std::cout<<A<<" "<<B<<" "<<rp(g)<<"\n"; 
+	freopen("s", "w", stdout); printf("%d", rand());
+	return 0;
 }
